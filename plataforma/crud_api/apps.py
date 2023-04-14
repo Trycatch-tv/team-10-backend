eb92edff4edff5d27a8ba34678ca92747e0962ec
briamcode <55761016+briamcode@users.noreply.github.com>
2023-04-14 17:08:28 -0500

    Delete plataforma directory

--- plataforma/crud_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-# configuración de la aplicación crud_api
-class CrudApiConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "crud_api"
