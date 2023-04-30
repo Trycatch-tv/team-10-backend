fa18e0c94b22e269dcce86dea71c0aa453be08b9
briamcode <55761016+briamcode@users.noreply.github.com>
2023-04-30 02:29:01 -0500

    Update

--- plataforma/build.sh
+++ plataforma/build.sh
@@ -2,13 +2,13 @@
 # exit on error
 set -o errexit
 
-#curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+
 pip install --upgrade pip
 pip install uvicorn
 pip install python-dotenv
 pip install -r requirements.txt
 
-#python3 manage.py collectstatic --no-input
+
 python manage.py makemigrations
 python manage.py migrate
-#python manage.py runserver
+
